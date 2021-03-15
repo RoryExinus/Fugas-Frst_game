a251e31ef7533a8648741cdc896c88383be6491d
RoryExinus <uzun_ak18@nuwm.edu.ua>
2021-03-15 15:12:51 +0200

    added Background

---
+++ Planes/Assets/Scripts/BackGroundLoop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundLoop : MonoBehaviour
+{
+    public float speed = 0.1f;
+    private Vector2 offset = Vector2.zero;
+    private Material material;
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        offset = material.GetTextureOffset("_MainTex");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset.y += speed + Time.deltaTime / 100;
+        material.SetTextureOffset("_MainTex", offset);
+    }
+}
