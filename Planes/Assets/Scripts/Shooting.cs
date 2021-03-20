c0e8b68a541e9952f6ca3c2a4b4e12ff334f9005
RoryExinus <uzun_ak18@nuwm.edu.ua>
2021-03-20 16:31:14 +0200

    minor fix

--- Planes/Assets/Scripts/Shooting.cs
+++ Planes/Assets/Scripts/Shooting.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+
     public Transform firePoint;
     public GameObject bulletPrefab;
+
+    public GameObject wallPrefab;
 
     public float bulletforce = 20f;
     public float fireRate = 1f;
@@ -25,7 +29,7 @@ public class Shooting : MonoBehaviour
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-
+        Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), wallPrefab.GetComponent<Collider2D>());
         rb.AddForce(firePoint.up * bulletforce, ForceMode2D.Impulse);
     }
 }
