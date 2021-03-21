12b4c071c588a3ddc541c258a9e7f45796074daa
RoryExinus <uzun_ak18@nuwm.edu.ua>
2021-03-22 01:56:40 +0200

    Added fire animation

--- Planes/Assets/Scripts/Bullet.cs
+++ Planes/Assets/Scripts/Bullet.cs
@@ -6,8 +6,11 @@ public class Bullet : MonoBehaviour
 {
     public float dmg;
 
+    public GameObject hitEffect;
     public void OnTriggerEnter2D(Collider2D collision)
     {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 0.3f);
         AsteroidScript aster = collision.GetComponent<AsteroidScript>();
         aster.TakeDamage(dmg);
         Destroy(gameObject);
