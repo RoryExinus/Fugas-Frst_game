4d7ed88702e07af07ec20afea3c7e91f4c0ed53b
Maxiiiik <shuprudkomax00@gmail.com>
2021-03-21 03:31:10 +0200

    [1.3] fix script Timer

--- Planes/Assets/Timer.cs
+++ Planes/Assets/Timer.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/*
+ треба дописати смерть.
+ тут вже є зупинка таймера (закоментовані частини).
+ */
+
+
 public class Timer : MonoBehaviour
 {
 
     public Text timerText;
     private float startTime;
+    // private bool finnished = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +25,24 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
+        if (finnished)
+            return;
+        */
+
         float t = Time.time - startTime;
 
         string minutes = ((int)t / 60).ToString();
-        string seconds = (t % 60).ToString("f2");
+        string seconds = (t % 60).ToString("f0");
 
         timerText.text = minutes + ":" + seconds;
     }
+
+    /*
+    public void Finnish()
+    {
+        finnished = true;
+        timerText.color = Color.red;
+    }
+    */
 }
